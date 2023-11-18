75d44c980da29b728357e71618fc5f019ef85425
Dinesh <dinesh2952002@outlook.com>
2023-11-18 21:05:32 +0530

    updated the code

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Navbar from './components/Navbar.js';
+import Marketplace from './components/Marketplace.js';
+import Profile from './components/Profile.js';
+import SellNFT from './components/SellNFT.js';
+import NFTPage from './components/NFTpage.js';
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="container">
+        <Routes>
+          <Route path="/" element={<Marketplace />}/>
+          <Route path="/nftPage" element={<NFTPage />}/>        
+          <Route path="/profile" element={<Profile />}/>
+          <Route path="/sellNFT" element={<SellNFT />}/>             
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
